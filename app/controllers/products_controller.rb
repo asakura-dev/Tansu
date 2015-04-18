c33d27e0c4aec88ac0635ad021155d7fc2bb5f18
asakura-dev <asakuradev@gmail.com>
2015-04-18 12:21:11 -0400

    Fix typo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,7 +35,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product)
-    elsep
+    else
       render 'edit'
     end
   end
